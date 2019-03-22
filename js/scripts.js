d17e4ef35827fcfa211ec3e6218a1a3ee0e73e95
josephmuema <jmuema95@gmail.com>
2019-03-22 10:52:49 +0300

    Finished up on the JavaScript code

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function triangle(){
   var side3=parseInt(c);
   var myArray=[side1,side2,side3]
 
+
+  if(side1===side2&&side2===side3&&side1===side3){
+    alert("the triangle is equilateral")
+    }
+    else if(side1!==side2&&side2!==side3&&side3!==side1){
+    alert("the triangle is Scalene")
+    }
+    else if(side1===side2&&side1!==side3||side2===side3&&side2!==side1||side1===side3&&side1!==side2&&side2+side1<side3&&side3+side2<side1&&side1+side3<side2){
+       alert("the triangle is isosceles")
+    }
+    else if(side2+side1<side3||side3+side2<side1||side1+side3<side2){
+    alert("it is not a triangle")
+    }
+    }
+    triangle();
