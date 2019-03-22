a2c016681a1b27e11cf325c83d0bb624627326b9
josephmuema <jmuema95@gmail.com>
2019-03-22 09:30:24 +0300

    Came up with the variables

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function triangle(){
+  var a =prompt('input-one');
+  var b =prompt('input-two');
+  var c =prompt('input-three');
+  var side1=parseInt(a);
+  var side2=parseInt(b);
+  var side3=parseInt(c);
+  var myArray=[side1,side2,side3]
+
