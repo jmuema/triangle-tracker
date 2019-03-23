9ccc1d28e6dd487f556a1dbd858d8d94b7593650
josephmuema <jmuema95@gmail.com>
2019-03-23 19:51:22 +0300

    corrected the error to my code in JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 function triangle() {
-  var triangle = [length, height, hypotenuse];
-  var length = parseInt(document.getElementById("length").value);
-  var height = parseInt(document.getElementById("height").value);
-  var hypotenuse = parseInt(document.getElementById("hypotenuse").value);
-  
-  
-      if (length===height && height===hypotenuse && hypotenuse===length) {
-        alert("Equilateral Triangle");
+var triangle = [length, height, hypotenuse];
+var length = parseInt(document.getElementById("length").value);
+var height = parseInt(document.getElementById("height").value);
+var hypotenuse = parseInt(document.getElementById("hypotenuse").value);
+
+
+    if (length===height && height===hypotenuse && hypotenuse===length) {
+      alert("Equilateral Triangle");
+}
+    else if (length===height || length===hypotenuse|| length===hypotenuse ) {
+      alert("Isosceles Triangle");
   }
-      else if (length===height || length===hypotenuse|| length===hypotenuse ) {
-        alert("Isosceles Triangle");
-    }
-    else if ((length+height)<=hypotenuse || (length+hypotenuse)<=height || (height+hypotenuse)<=length) {
-      alert("NOT a Triangle");
-    }
-    else if (length!==height && height!==hypotenuse && length!=hypotenuse) {
-      alert("Scalene Triangle");
-    }
-    
-    }
+  else if ((length+height)<=hypotenuse || (length+hypotenuse)<=height || (height+hypotenuse)<=length) {
+    alert("NOT a Triangle");
   }
-  
+  else if (length!==height && height!==hypotenuse && length!=hypotenuse) {
+    alert("Scalene Triangle");
+  }
+  else {
+    alert("error");
+  }
+}
